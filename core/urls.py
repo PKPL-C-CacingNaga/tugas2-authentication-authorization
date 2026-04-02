fd755b9a96969772df69f87f297c618343ae2450
w7ldan <wildanhidayatirl@gmail.com>
2026-04-02 20:29:18 +0700

    fix: removed default fallback when cancelling OAuth login

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/3rdparty/login/cancelled/', RedirectView.as_view(url='/', permanent=False)),
     path('accounts/', include('allauth.urls')),
     path('', include('main.urls')),
     path('settings/', include('theme.urls')),
